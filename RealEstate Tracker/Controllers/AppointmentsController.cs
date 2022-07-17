2e2b1819262dd9ccad2af1ca71949f6554ee2479
Arya Chauhan <67852318+chauhanarya007@users.noreply.github.com>
2022-07-17 17:17:21 -0400

    Added authentication for Appointments and nav link

--- RealEstate Tracker/Controllers/AppointmentsController.cs
+++ RealEstate Tracker/Controllers/AppointmentsController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
@@ -10,6 +11,7 @@ using RealEstate_Tracker.Models;
 
 namespace RealEstate_Tracker.Controllers
 {
+    [Authorize]
     public class AppointmentsController : Controller
     {
         private readonly ApplicationDbContext _context;
