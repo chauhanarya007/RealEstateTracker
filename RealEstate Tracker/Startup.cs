21015b69f51efba798920f227e14d02f38b980d0
Arya Chauhan <67852318+chauhanarya007@users.noreply.github.com>
2022-07-19 02:24:58 -0400

    Added google login after updating new credentials

--- RealEstate Tracker/Startup.cs
+++ RealEstate Tracker/Startup.cs
@@ -34,6 +34,14 @@ namespace RealEstate_Tracker
 
             services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = false)
                 .AddEntityFrameworkStores<ApplicationDbContext>();
+            services.AddAuthentication()
+                .AddGoogle(options => {
+                    options.ClientId = Configuration.GetSection("Authentication:Google")["ClientId"];
+                    options.ClientSecret = Configuration.GetSection("Authentication:Google")["ClientSecret"];
+                });
+
+
+            services.AddSession();
             services.AddControllersWithViews();
         }
 
